bac40bc33205bd68468983e4392b55fa61188cc6
rahulleoak <43439584+rahulleoak@users.noreply.github.com>
2019-03-22 03:52:57 -0700

    Snake Game File
    Snake C# file w/ mapping

---
+++ Package/Snake/Snake/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snake
+{
+    class Circle
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Circle()
+        {
+            //set X and Y to 0 upon calling
+            X = 0;
+            Y = 0;
+        }
+    }
+}
---
+++ Package/Snake/Snake/Settings.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public enum Directions
+    {
+        //identify as well as classify the various directions of movement
+        Left,
+        Right,
+        Up,
+        Down
+    };
+
+    class Settings
+    {
+        public static int Width {get;set;}  // sets width dimension
+        public static int Height {get;set;} //sets height dimension
+        public static int Speed {get;set;} //sets Speed of movement
+        public static int Score {get;set;} //sets Score
+        public static int Points { get; set; } //sets Points
+        
+        public static bool Gameover {get;set;} //used to indicate if game is over or not
+        public static Directions direction {get;set;}
+
+        public Settings()
+        {
+            //assigning value to the variables upon calling settings
+
+            Width = 16;
+            Height = 16;
+            Speed = 20;
+            Score = 0;
+            Points = 100;
+            Gameover = false;
+            direction = Directions.Down;
+        }
+    }
+}
